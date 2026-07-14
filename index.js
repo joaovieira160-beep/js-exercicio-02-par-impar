d9e96eb6d5a09c56811bc166262e89493712171d
joao <joao_vieira160@estudante.sesisenai.org.br>
2026-07-14 14:23:24 -0300

    commit do index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+let numero = parseInt(prompt("escolha um numero:"))
+
+if(numero % 2 ===0){
+    console.log("o numero " + numero + "é par ")
+}else{
+    console.log("o numero" + numero + " é impar")
+}
